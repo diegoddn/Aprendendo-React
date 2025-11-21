3498daa66b49f9f1f382ba8bc8a3ee32e163b818
diegoddn <dhiego.natividade@hotmail.com>
2025-11-21 15:13:48 -0300

    Initial commit

---
+++ projeto-react/src/App.jsx
@@ -0,0 +1,16 @@
+import CardProduto from "./components/CardProduto";
+
+function App() {
+  return (
+    <div>
+      <CardProduto />
+      <CardProduto />
+      <CardProduto />
+      <CardProduto />
+      <CardProduto />
+      <CardProduto />
+    </div>
+  );
+}
+
+export default App;
---
+++ projeto-react/src/components/CardProduto.jsx
@@ -0,0 +1,12 @@
+const CardProduto = () => {
+  return (
+    <div>
+      <h1>PC Gamer</h1>
+      <h2>Computador para trabalho, estudos e jogos</h2>
+      <p>R$ 6.000,00</p>
+      <button>Comprar</button>
+    </div>
+  );
+};
+
+export default CardProduto;
